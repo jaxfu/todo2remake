a2fbd1516e09794c073ff52945cb7122f0ded8e8
Jackson Furr <jackson.a.furr@gmail.com>
2024-07-19 12:13:30 -0400

    addTodo route working

--- server/internal/routing/routes/todos.go
+++ server/internal/routing/routes/todos.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"fmt"
 	"furrj/todo_2_remake/internal/types"
 	"net/http"
 
@@ -19,9 +20,29 @@ func GetTodos() gin.HandlerFunc {
 	}
 }
 
+type ResponseValid struct {
+	Valid bool `json:"valid"`
+}
+
 func AddTodo() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, testTodos)
+		var payload types.Todo
+		response := ResponseValid{
+			Valid: false,
+		}
+
+		if err := ctx.BindJSON(&payload); err != nil {
+			fmt.Printf("Error binding payload: %+v\n", err)
+			ctx.JSON(http.StatusInternalServerError, response)
+			return
+		}
+		fmt.Printf("Login Payload: %+v\n", payload)
+
+		payload.ID = uint(len(testTodos) + 1)
+		testTodos = append(testTodos, payload)
+		response.Valid = true
+
+		ctx.JSON(http.StatusOK, response)
 	}
 }
 
